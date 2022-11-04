a8f0afefb4714f7d326ccc6e5175d59fecb9da79
Sunny Goyal <goyalsunny888@gmail.com>
2022-11-04 22:28:17 +0530

    Update findMissingNumber.java

--- Arrays/findMissingNumber.java
+++ Arrays/findMissingNumber.java
@@ -1,8 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
         // Method2 - Using XOR
-        // [3,0,1] - 3 ^ 0 ^ 0 ^ 1 ^ 1 ^ 2
-        // Rearrange - 0 ^ 0 ^ 1 ^ 1 ^ 2 => 0 ^ 2 => 2
+        // [3,0,1] - 3 ^ 3 ^ 0 ^ 0 ^ 1 ^ 1 ^ 2
+        // Rearrange - 0 ^ 0 ^ 1 ^ 1 ^ 2 ^ 3 ^ 3 => 0 ^ 2 => 2
         int res = nums.length;
         for(int i=0; i<nums.length; i++){
             res = res ^ i ^ nums[i]; // a^b^b = a

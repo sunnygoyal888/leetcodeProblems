6ec86a1ad501b7c4925420ba7bfb40faa806c37b
Sunny Goyal <goyalsunny888@gmail.com>
2022-11-06 22:07:44 +0530

    Update oddEvenLL.java

--- LinkedList/oddEvenLL.java
+++ LinkedList/oddEvenLL.java
@@ -25,3 +25,35 @@ class Solution {
         return head;
     }
 }
+
+// Modified Question
+/* If order of odd and even is not alternative i.e odd and even numbers are given in random order in Question
+   Output - even numbers should appear before all the odd numbers in the modified list
+   Input: 17 -> 15 -> 8 -> 9 -> 2 -> 4 -> 6 -> NULL
+   Output: 8 -> 2 -> 4 -> 6 -> 17 -> 15 -> 9
+*/
+class Solution{
+    Node evenOddList(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node even = new Node(-1);
+        Node odd = new Node(-1);
+        Node evenHead = even;
+        Node oddHead = odd;
+        
+        while(head != null){
+            if(head.data%2 == 0){
+                even.next = head;
+                even = even.next;
+            }else{
+                odd.next = head;
+                odd = odd.next;
+            }
+            head = head.next;
+        }
+        odd.next = null;
+        even.next = oddHead.next;
+        return evenHead.next;
+    }
+}

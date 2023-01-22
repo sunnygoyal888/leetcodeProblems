8be2f9e36205de20a7a29835da9926da86db6c34
Sunny Goyal <goyalsunny888@gmail.com>
2023-01-22 14:20:02 +0530

    Create sameTrees.java

---
+++ BinaryTrees/sameTrees.java
@@ -0,0 +1,41 @@
+// Question link -> https://leetcode.com/problems/same-tree/description/
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+class Solution {
+    // Recursive Approach | TC - O(n) | SC - O(n)
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+  
+    /* Iterative Approach | TC - O(n)
+    public boolean isSameTree(TreeNode p, TreeNode q) {        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()){
+            TreeNode f = queue.poll();
+            TreeNode s = queue.poll();
+            if(f == null && s == null){
+                continue;
+            }else if(f == null || s == null || f.val != s.val){
+                return false;
+            }
+            queue.add(f.left);
+            queue.add(s.left);
+            queue.add(f.right);
+            queue.add(s.right);
+        }
+        return true;
+    }
+    */
+}
